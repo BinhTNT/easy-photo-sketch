609f8138d5066e0715e01b83d17a864b838ba939
BinhTNT <binh.tangocthai@gmail.com>
2019-04-08 17:17:13 +0700

    Added Info page

---
+++ EasyPhotoSketch/EasyPhotoSketch/InfoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+namespace EasyPhotoSketch
+{
+	public partial class InfoPage : ContentPage
+	{
+		public InfoPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void CloseButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
--- EasyPhotoSketch/EasyPhotoSketch/MainPage.xaml.cs
+++ EasyPhotoSketch/EasyPhotoSketch/MainPage.xaml.cs
@@ -11,5 +11,10 @@ namespace EasyPhotoSketch
             BindingContext = new MainViewModel();
             MainViewModel.SetMainPage(this);
         }
+
+        private async void ViewInfoButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new InfoPage());
+        }
     }
 }
