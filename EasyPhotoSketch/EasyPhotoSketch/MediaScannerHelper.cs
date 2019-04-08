a92fbe4b3fa06d090f3a159c689dd18ff03b2881
BinhTNT <binh.tangocthai@gmail.com>
2019-04-08 14:37:21 +0700

    Make sketched image available in Gallery by invoke Media scanner

---
+++ EasyPhotoSketch/EasyPhotoSketch/MediaScannerHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace EasyPhotoSketch
+{
+
+    public class MediaScannerHelper
+    {
+        public delegate void ScanMediaFileCallback(string filePath);
+        private ScanMediaFileCallback m_scanMediaFileCallback = null;
+        private static MediaScannerHelper instance = null;
+        public static MediaScannerHelper Instance()
+        {
+            if (instance == null)
+            {
+                instance = new MediaScannerHelper();
+            }
+            return instance;
+        }
+
+        public void SetScanMediaFileCallback(ScanMediaFileCallback scanMediaFileCallback)
+        {
+            m_scanMediaFileCallback = scanMediaFileCallback;
+        }
+
+        public void ScanMediaFile(string filePath)
+        {
+            m_scanMediaFileCallback?.Invoke(filePath);          
+        }
+    }
+}
